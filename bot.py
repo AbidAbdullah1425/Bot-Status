2b4b751a6059a35f260e12b6622f7b94a1f08c93
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-12-13 20:35:46 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,19 +9,19 @@ from datetime import datetime
 import pyrogram.utils
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, CHANNEL_ID, PORT
+from config import API_HASH, API_ID, LOGGER, BOT_TOKEN, BOT_WORKERS, UPDATE_CHANNEL, PORT
 
 class Bot(Client):
     def __init__(self):
         super().__init__(
             name="Bot",
             api_hash=API_HASH,
-            api_id=APP_ID,
+            api_id=API_ID,
             plugins={
                 "root": "plugins"
             },
-            workers=TG_BOT_WORKERS,
-            bot_token=TG_BOT_TOKEN
+            workers=BOT_WORKERS,
+            bot_token=BOT_TOKEN
         )
         self.LOGGER = LOGGER
 
@@ -31,13 +31,13 @@ class Bot(Client):
         self.uptime = datetime.now()
 
         try:
-            db_channel = await self.get_chat(CHANNEL_ID)
+            db_channel = await self.get_chat(UPDATE_CHANNEL)
             self.db_channel = db_channel
             test = await self.send_message(chat_id=db_channel.id, text="Test Message")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make sure the bot is an Admin in the DB Channel, and double-check the CHANNEL_ID value. Current value: {CHANNEL_ID}")
+            self.LOGGER(__name__).warning(f"Make sure the bot is an Admin in the DB Channel, and double-check the CHANNEL_ID value. Current value: {UPDATE_CHANNEL}")
             self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
             sys.exit()
 
