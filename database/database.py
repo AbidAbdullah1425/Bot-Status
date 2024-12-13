3f5c5ec2f2ecbb3a7bf889ed859e3f898315b2e2
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-12-13 20:49:40 +0600

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
-from config import DB_URI, DB_NAME
+from config import DB_URL, DB_NAME
 
 class Database:
     def __init__(self):
-        self.client = MongoClient(DB_URI)
+        self.client = MongoClient(DB_URL)
         self.db = self.client[DB_NAME]
 
     def get_collection(self, name):
