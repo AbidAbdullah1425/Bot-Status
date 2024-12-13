42ee7bb7099aa6cecc9ea86b3541594510f31dad
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-12-13 20:46:11 +0600

    Update config.py

--- config.py
+++ config.py
@@ -18,6 +18,8 @@ UPDATE_CHANNEL = int(os.environ.get("UPDATE_CHANNEL", "-1002355785538"))
 OWNER = int(os.environ.get("OWNER", "5296584067"))
 BOT_WORKERS = int(os.environ.get("BOT_WORKERS", "4"))
 
+PORT = os.environ.get("PORT", "8080")
+
 # Set Up Logger
 LOG_FILE_NAME = "botstatus.txt"
 
