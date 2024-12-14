c6bf19feb393c5fea0f09c58bc164014a7babdae
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-12-14 13:15:07 +0600

    Update database.py

--- database/database.py
+++ database/database.py
@@ -6,11 +6,16 @@ class Database:
         self.client = MongoClient(DB_URL)
         self.db = self.client[DB_NAME]
 
-    def get_collection(self, name):
-        return self.db[name]
-
     def insert_or_update(self, collection, query, update, upsert=False):
         """Insert a new document or update an existing one."""
-        collection = self.get_collection(collection)
-        result = collection.update_one(query, update, upsert=upsert)
+        result = self.db[collection].update_one(query, update, upsert=upsert)
         return result
+
+    def delete_one(self, collection, query):
+        """Delete a document."""
+        result = self.db[collection].delete_one(query)
+        return result
+
+    def find_all(self, collection):
+        """Find all documents in a collection."""
+        return list(self.db[collection].find())
